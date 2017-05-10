c78b68982604c8939fbb7dc58f41bb829dbcdacc
Ewelina <Ewelina@DESKTOP41>
2017-05-10 11:26:40 +0200

    .

--- Obiektowa baza danayc/MainWindow.xaml.cs
+++ Obiektowa baza danayc/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace Obiektowa_baza_danych
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// </summary>   
 
     public partial class MainWindow : Window
     {
